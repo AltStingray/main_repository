a25021cc4ca7a063c2bbe57f71b1a3c19c1daab7
Alt <egoroff2206@gmail.com>
2025-02-15 16:15:26 +0000

    Files transfered and orginized by folders

--- User_Authentication_System/hashing.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-def encryption(data):
-    data = bytes(data, "utf-8")
-    encrypted_data = hashlib.sha3_256(data) 
-    hex_representation = encrypted_data.hexdigest()
-    return hex_representation

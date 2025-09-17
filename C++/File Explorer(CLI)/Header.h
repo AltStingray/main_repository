11975935a871cd2feb231cfb0f89226978738de7
Alt <egoroff2206@gmail.com>
2025-09-17 17:59:18 +0100

    Update Header.h

--- C++/File Explorer(CLI)/Header.h
+++ C++/File Explorer(CLI)/Header.h
@@ -2,15 +2,22 @@
 #include <iostream>
 
 std::string commands_instruction{
-"\nDirectory navigation: \n\
+"\n\
+Directory navigation: \n\
 cd (directory) - to navigate directories.\n\
 cd / - to navigate into the root directory.\n\
 cd .. - to navigate back in the directories.\n\
-ls - to display files in the current directory.\n\n\
-\
+ls - to display files in the current directory.\n\
+\n\
 File operations: \n\
-mkdir (directory) - to create a new directory.\n\n\
-\
+mkdir - to create a new directory.\n\
+touch - to create a new file.\n\
+rm - to delete an existing file.\n\
+rmdir - to delete an existing empty directory.\n\
+mv - to move a file/directory or to rename it (i.e. cp file destination OR cp oldfile newfile)\n\
+cp - to copy a file (i.e. cp oldfile destination/filename)\n\
+open - to open up an existing file.\n\
+\n\
 System commands: \n\
 clear - to clear the terminal.\n\
 exit - to terminate application.\n"
